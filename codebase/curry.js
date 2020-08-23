4cdf5a662092ad1604ab7d5bdd532f83023ba519
jiangwenbin <wenbin.jiang@coollu.com.cn>
2020-08-23 21:24:13 +0800

    同步函数部分

--- codebase/curry.js
+++ codebase/curry.js
@@ -1,24 +1,57 @@
-function add() {
-    var args = [].slice.call(arguments)
-    return function() {
-        var newArgs = [].slice.call(arguments)
-        var allArg = args.concat(newArgs)
-        return allArg.reduce((prev, curr) => {
-            return prev + curr
-        }, 0)
-    }
+function curry(fn, firstArg) {
+  // 返回一个新函数
+  return function() {
+    // 新函数调用时会继续传参
+    var restArgs = [].slice.call(arguments)
+    // 参数合并，通过apply调用原函数
+    return fn.apply(this, [firstArg, ...restArgs])
+  }
 }
 
-add(1,2,3) // 6
-add(1) // 1
-add(1)(2) // 3
-add(1)(2)(3) // 6
-add(1, 2)(3) // 6
-
-for (let index = 0; index < 5; index++) {
-    function test() {
-        return index;
-    }
+function curry(fn) {
+  // 保存预置参数
+  var presetArgs = [].slice.call(arguments, 1)
+  // 返回一个新函数
+  return function() {
+    // 新函数调用时会继续传参
+    var restArgs = [].slice.call(arguments)
+    // 参数合并，通过apply调用原函数
+    return fn.apply(this, [...presetArgs, ...restArgs])
+  }
 }
-var result = test()
-console.log(index)
+
+function curry(fn) {
+  // 获取原函数的参数长度
+  const argLen = fn.length;
+  // 保存预置参数
+  const presetArgs = [].slice.call(arguments, 1)
+  // 返回一个新函数
+  return function() {
+    // 新函数调用时会继续传参
+    const restArgs = [].slice.call(arguments)
+    const allArgs = [...presetArgs, ...restArgs]
+    if (allArgs.length >= argLen) {
+      // 如果参数够了，就执行原函数
+      return fn.apply(this, allArgs)
+    } else {
+      // 否则继续柯里化
+      return curry.call(null, fn, ...allArgs)
+    }
+  }
+}
+
+function curry(fn) {
+  // 保存预置参数
+  const presetArgs = [].slice.call(arguments, 1)
+  // 返回一个新函数
+  function curried () {
+    // 新函数调用时会继续传参
+    const restArgs = [].slice.call(arguments)
+    const allArgs = [...presetArgs, ...restArgs]
+    return curry.call(null, fn, ...allArgs)
+  }
+  curried.toString = function() {
+    return fn.apply(null, presetArgs)
+  }
+  return curried;
+}
