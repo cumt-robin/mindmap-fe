bf74c8b35cb9cfa90f9d4dc4de14b8b4f583f815
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-12 14:46:16 +0800

    fix: alg 虚拟节点应用

--- codebase/alg/LinkedList/medium/addTwoNumbers.js
+++ codebase/alg/LinkedList/medium/addTwoNumbers.js
@@ -2,7 +2,7 @@
  * @Author: 蒋文斌
  * @Date: 2021-03-12 13:03:56
  * @LastEditors: 蒋文斌
- * @LastEditTime: 2021-03-12 14:40:22
+ * @LastEditTime: 2021-03-12 14:45:45
  * @Description: 自动生成
  */
 
@@ -28,16 +28,16 @@ function arr2ListHead(arr) {
     if (arr.length === 0) {
         return null;
     }
-    var listNode = new ListNode(arr[0])
-    var i = 1;
-    var tempNode = listNode;
+    var dummy = {}
+    var i = 0;
+    var tempNode = dummy;
     while(i < arr.length) {
         var nextNode = new ListNode(arr[i])
         tempNode.next = nextNode;
         tempNode = nextNode;
         i++;
     }
-    return listNode;
+    return dummy.next;
 }
 
 var l1 = new arr2ListHead(input1)
@@ -48,6 +48,7 @@ var l2 = new arr2ListHead(input2)
  * 做这个题的时候陷入了一个误区，我以为只能用链表做，但最后才发现应该结合其他数据结构来做，比如栈。
  * 虽然性能很差，但总算做出来了。
  * 也学到一个技巧，要翻转链表，可以用栈。
+ * 最后还可以用虚拟节点，来构造一个可以完全递归的对象
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
@@ -76,12 +77,9 @@ var addTwoNumbers = function (l1, l2) {
     if (carry > 0) {
         resultStack.unshift(1);
     }
-    var listNode = {
-        val: resultStack[0],
-        next: null,
-    };
-    var tempNode = listNode;
-    var i = 1;
+    var dummy = {}
+    var tempNode = dummy;
+    var i = 0;
     while (i < resultStack.length) {
         tempNode.next = {
             val: resultStack[i],
@@ -90,5 +88,5 @@ var addTwoNumbers = function (l1, l2) {
         tempNode = tempNode.next;
         i++;
     }
-    return listNode;
+    return dummy.next;
 };
