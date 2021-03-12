769331b64c4d6ad3fea3ff795dd9c73e32aee7b3
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-12 21:44:42 +0800

    feat: 删除链表的节点

---
+++ codebase/alg/LinkedList/easy/deleteNodeByHeadAndVal.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-12 21:41:49
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-12 21:42:29
+ * @Description: 自动生成
+ */
+
+// https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNodeByHeadAndVal = function(head, val) {
+
+};
