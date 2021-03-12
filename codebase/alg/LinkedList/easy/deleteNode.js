c6526db7132eabbee28e084b86fe077552194218
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-12 08:51:07 +0800

    feat: alg deleteNode

--- codebase/alg/LinkedList/easy/deleteNode.js
+++ codebase/alg/LinkedList/easy/deleteNode.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-12 08:41:48
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-12 08:50:43
+ * @Description: 自动生成
+ */
 
 // https://leetcode-cn.com/problems/delete-middle-node-lcci/
 
@@ -8,10 +15,12 @@
  *     this.next = null;
  * }
  */
+
 /**
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-var deleteNode = function(node) {
-    
+ var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
 };
