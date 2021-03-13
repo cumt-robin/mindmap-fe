7ebc1d1be45a929584724bbcfc5dfcf99bc7b617
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-13 18:59:52 +0800

    feat: alg middleNode

--- codebase/alg/LinkedList/easy/middleNode.js
+++ codebase/alg/LinkedList/easy/middleNode.js
@@ -2,23 +2,23 @@
  * @Author: 蒋文斌
  * @Date: 2021-03-13 11:54:03
  * @LastEditors: 蒋文斌
- * @LastEditTime: 2021-03-13 11:54:13
+ * @LastEditTime: 2021-03-13 18:59:35
  * @Description: 自动生成
  */
 
 // https://leetcode-cn.com/problems/middle-of-the-linked-list/
 
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
+ * 快慢指针还是好用
  * @param {ListNode} head
  * @return {ListNode}
  */
- var middleNode = function(head) {
-
+var middleNode = function(head) {
+    var slow = head;
+    var fast = head;
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
 };
