bdfbc821141b4f0d89c4a5e8a929a2c7b309f1e9
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-13 11:54:35 +0800

    feat: alg deleteNodeByHeadAndVal

--- codebase/alg/LinkedList/easy/deleteNodeByHeadAndVal.js
+++ codebase/alg/LinkedList/easy/deleteNodeByHeadAndVal.js
@@ -2,7 +2,7 @@
  * @Author: 蒋文斌
  * @Date: 2021-03-12 21:41:49
  * @LastEditors: 蒋文斌
- * @LastEditTime: 2021-03-12 21:42:29
+ * @LastEditTime: 2021-03-13 11:52:16
  * @Description: 自动生成
  */
 
@@ -16,10 +16,27 @@
  * }
  */
 /**
+ * 没啥技巧，主要是prev和curr的迭代
+ * newHead这里就比较简单，只要删的不是头节点，newHead就直接是头节点；否则就要处理下
  * @param {ListNode} head
  * @param {number} val
  * @return {ListNode}
  */
-var deleteNodeByHeadAndVal = function(head, val) {
-
+var deleteNode = function(head, val) {
+    var prev = null;
+    var curr = head;
+    var newHead = head;
+    while(curr !== null) {
+        if (curr.val === val) {
+            if (prev === null) {
+                newHead = curr.next;
+            } else {
+                prev.next = curr.next;
+            }
+            break;
+        }
+        prev = curr;
+        curr = curr.next;
+    }
+    return newHead
 };
---
+++ codebase/alg/LinkedList/easy/middleNode.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-13 11:54:03
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-13 11:54:13
+ * @Description: 自动生成
+ */
+
+// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {
+
+};
