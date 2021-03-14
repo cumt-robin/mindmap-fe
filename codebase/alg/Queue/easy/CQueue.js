e15a7fdef93550e4d95e45b7b156880cc4df2698
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-14 18:14:26 +0800

    feat: alg fib

--- codebase/alg/Queue/easy/CQueue.js
+++ codebase/alg/Queue/easy/CQueue.js
@@ -2,14 +2,18 @@
  * @Author: 蒋文斌
  * @Date: 2021-03-13 22:15:22
  * @LastEditors: 蒋文斌
- * @LastEditTime: 2021-03-13 22:28:58
+ * @LastEditTime: 2021-03-14 15:11:03
  * @Description: 自动生成
  */
 
 // https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
 
+/**
+ * 一开始还没了解双栈的玩法，直接用的数组，用unshift保证先进的在前面，用pop保证先进的先出。
+ * 最下面附上了双栈的玩法
+ */
 var CQueue = function() {
-
+    this.stack = [];
 };
 
 /** 
@@ -17,14 +21,54 @@ var CQueue = function() {
  * @return {void}
  */
 CQueue.prototype.appendTail = function(value) {
-
+    this.stack.unshift(value)
+    return void 0;
 };
 
 /**
  * @return {number}
  */
 CQueue.prototype.deleteHead = function() {
+    if (this.stack.length > 0) {
+        return this.stack.pop();
+    } else {
+        return -1;
+    }
+};
 
+/**
+ * 两个栈，一个负责插入，一个负责删除
+ * 注意栈只能push和pop
+ */
+var CQueue = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+};
+
+/** 
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function(value) {
+    this.stack1.push(value)
+    return void 0;
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function() {
+    if (this.stack2.length === 0) {
+        // 把stack1的先全部出栈，依次入栈到stack2
+        while(this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop())
+        }
+    }
+    if (this.stack2.length > 0) {
+        return this.stack2.pop();
+    } else {
+        return -1;
+    }
 };
 
 /**
---
+++ codebase/alg/Recursion/easy/fib.js
@@ -0,0 +1,58 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-14 15:14:16
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-14 18:06:27
+ * @Description: 自动生成
+ */
+
+// https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+
+/**
+ * 是解题的逻辑，但是递归爆栈了！！！可以考虑尾递归优化
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n === 0) {
+        return 0
+    } else if (n === 1) {
+        return 1
+    } else {
+        return fib(n - 1) + fib(n - 2)
+    }
+};
+
+/**
+ * 尾递归优化，避免了调用栈的激增；这里的思路是把所有的已知量和每一步的运算结果带进去作为尾递归的参数。
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    function fibInner(n, a1, a2) {
+        return n === 0 ? a1 : fibInner(n - 1, a2, a1 + a2)
+    }
+    return fibInner(n, 0, 1)
+};
+
+/**
+ * 记住前两个值，求和
+ * 为啥取模是 1e9+7 ，没太懂？防止求和溢出，所以找一个32位内的较大质数
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n === 0 || n === 1) {
+        return n
+    } else {
+        var a1 = 0;
+        var a2 = 1;
+        var curr;
+        for (var i = 2; i <= n; i++) {
+            curr = (a1 + a2) % (1e9+7);
+            a1 = a2;
+            a2 = curr
+        }
+        return curr;
+    }
+};
