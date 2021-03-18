7a45776653bad1cd10f483441e332d72421f7ada
cumt-robin <1148121254@qq.com>
2021-03-19 00:25:08 +0800

    feat: alg backspaceCompare

--- codebase/alg/Stack/easy/backspaceCompare.js
+++ codebase/alg/Stack/easy/backspaceCompare.js
@@ -24,11 +24,33 @@ class Stack {
     }
 }
 
+function handleStack(S) {
+    var stack = []
+    for (let index = 0; index < S.length; index++) {
+        var c = S[index]
+        if (c === '#') {
+            stack.pop();
+        } else {
+            stack.push(c);
+        }
+    }
+    return stack;
+}
+
 /**
  * @param {string} S
  * @param {string} T
  * @return {boolean}
  */
 var backspaceCompare = function (S, T) {
-
+    var stack1 = handleStack(S)
+    var stack2 = handleStack(T);
+    while(stack1.length > 0 || stack2.length > 0) {
+        var s1 = stack1.pop();
+        var s2 = stack2.pop();
+        if (s1 !== s2) {
+            return false;
+        }
+    }
+    return true;
 };
