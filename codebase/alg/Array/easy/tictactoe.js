f3406bc74a691e8e3c0e4a9edbb65d7147cd46b1
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-20 21:26:16 +0800

    feat: alg removeOuterParentheses

---
+++ codebase/alg/Array/easy/tictactoe.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-16 13:48:19
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-16 13:48:32
+ * @Description: 自动生成
+ */
+
+// https://leetcode-cn.com/problems/find-winner-on-a-tic-tac-toe-game/
+
+/**
+ * @param {number[][]} moves
+ * @return {string}
+ */
+ var tictactoe = function(moves) {
+
+};
--- codebase/alg/Stack/easy/removeOuterParentheses.js
+++ codebase/alg/Stack/easy/removeOuterParentheses.js
@@ -1,10 +1,60 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-20 20:56:42
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-20 21:25:25
+ * @Description: 自动生成
+ */
 
 // https://leetcode-cn.com/problems/remove-outermost-parentheses/
 
+class Stack {
+    constructor() {
+        this.list = []
+    }
+
+    get size() {
+        return this.list.length;
+    }
+
+    empty() {
+        return this.size === 0
+    }
+
+    push(val) {
+        this.list.push(val)
+    }
+
+    pop() {
+        return this.list.pop();
+    }
+
+    peek() {
+        return this.list[this.list.length - 1]
+    }
+}
+
 /**
+ * 这题目说实话有点扯淡，搞了个自己的原语概念出来。
  * @param {string} S
  * @return {string}
  */
 var removeOuterParentheses = function(S) {
-
+    const stack = new Stack()
+    let newStr = ''
+    for (let index = 0; index < S.length; index++) {
+        const char = S[index];
+        if (char === '(') {
+            stack.push(char)
+            if (stack.size > 1) {
+                newStr += "("
+            }
+        } else {
+            stack.pop();
+            if (stack.size > 0) {
+                newStr += ")"
+            }
+        }
+    }
+    return newStr;
 };
