db699477ad5c5512af57fae98afd940e9c65bda9
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-21 10:48:16 +0800

    feat: alg tictactoe暴力解法

--- codebase/alg/Array/easy/tictactoe.js
+++ codebase/alg/Array/easy/tictactoe.js
@@ -2,28 +2,44 @@
  * @Author: 蒋文斌
  * @Date: 2021-03-16 13:48:19
  * @LastEditors: 蒋文斌
- * @LastEditTime: 2021-03-20 22:35:49
+ * @LastEditTime: 2021-03-21 10:47:52
  * @Description: 自动生成
  */
 
 // https://leetcode-cn.com/problems/find-winner-on-a-tic-tac-toe-game/
 
+// 暴力解法
 function checkWinner(chessboard) {
     const winModel = [
-        [0,0], [0,1], [0,2],
-        [1,0], [1,1], [1,2],
-        [2,0], [2,1], [2,2],
-        [0,0], [0,1], [0,2],
-        [1,0], [1,1], [1,2],
-        [2,0], [2,1], [2,2],
-        [0,0], [1,1], [2,2],
-        [2,0], [1,1], [0,2],
+        [[0,0], [0,1], [0,2]],
+        [[1,0], [1,1], [1,2]],
+        [[2,0], [2,1], [2,2]],
+        [[0,0], [1,0], [2,0]],
+        [[0,1], [1,1], [2,1]],
+        [[0,2], [1,2], [2,2]],
+        [[0,0], [1,1], [2,2]],
+        [[2,0], [1,1], [0,2]],
     ]
-    for (let index = 0; index < winModel.length; index++) {
-        const position = winModel[index]
-        // if (chessboard[]) {
-
-        // }
+    let isWinnerA = false;
+    let isWinnerB = false;
+    for (let i = 0; i < winModel.length; i++) {
+        const positions = winModel[i]
+        isWinnerA = positions.every(p => {
+            const value = chessboard[p[0]][p[1]]
+            return value === 'A'
+        })
+        if (isWinnerA) {
+            const p = positions[0]
+            return chessboard[p[0]][p[1]]
+        }
+        isWinnerB = positions.every(p => {
+            const value = chessboard[p[0]][p[1]]
+            return value === 'B'
+        })
+        if (isWinnerB) {
+            const p = positions[0]
+            return chessboard[p[0]][p[1]]
+        }
     }
 }
 
@@ -36,10 +52,15 @@ var tictactoe = function(moves) {
     let role = 'A'
     for (let index = 0; index < moves.length; index++) {
         const move = moves[index];
-        chessboard[move[0], move[1]] = role
-        role === 'A' ? 'B' : 'A'
+        chessboard[move[0]][move[1]] = role
+        role = role === 'A' ? 'B' : 'A'
     }
-    if (chessboard) {
-
+    const winner = checkWinner(chessboard)
+    if (winner) {
+        return winner
+    } else if (moves.length === 9) {
+        return 'Draw'
+    } else {
+        return 'Pending'
     }
 };
