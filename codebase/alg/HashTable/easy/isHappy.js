a64e222c27a36053a791bbbafb9d01adc6ca41f2
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-21 21:59:35 +0800

    feat: alg isHappy

---
+++ codebase/alg/HashTable/easy/isHappy.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-21 19:39:45
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-21 21:59:16
+ * @Description: 自动生成
+ */
+
+// https://leetcode-cn.com/problems/happy-number/
+
+// function splitNumer(num) {
+//     var result = [];
+//     while (num >= 10) {
+//         var first = num / 10;
+//         num = 
+//     }
+// }
+
+// /**
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// var isHappy = function(n) {
+//     // 准备一个求值的集合，用于检测死循环，返回false；如果求值得到了1，则返回true
+//     // 写一个拆数值的方法
+//     var 
+// };
