585bd5ca5068ea220ea73aed7672c4e8c45c262e
jiangwenbin <wenbin.jiang@coollu.com.cn>
2021-03-29 20:36:53 +0800

    feat: intersect

---
+++ codebase/数据结构和算法/Sort/intersect.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: 蒋文斌
+ * @Date: 2021-03-28 20:44:06
+ * @LastEditors: 蒋文斌
+ * @LastEditTime: 2021-03-28 20:44:17
+ * @Description: 自动生成
+ */
+
+// https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+
+};
