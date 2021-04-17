3152702f178e71d064e8d9531d76f1a36bd707f7
cumt-robin <1148121254@qq.com>
2021-04-17 16:26:13 +0800

    feat: quickSort

--- 代码/数据结构和算法/Sort/basic/quickSort.js
+++ 代码/数据结构和算法/Sort/basic/quickSort.js
@@ -1,3 +1,30 @@
-function quickSort(arr) {
 
+/**
+ * 快排利用了Divide and Conquer（分治）的思想
+ * @param {Array} arr
+ * @returns {Array} result
+ */
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    // 选一个随机的元素出来
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const middleItem = arr[randomIndex]
+    const left = [];
+    const right = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (i === randomIndex) {
+            // 排除掉自己
+            continue;
+        }
+        const element = arr[i];
+        // 小的放左边，大的放右边
+        if (element <= middleItem) {
+            left.push(element)
+        } else {
+            right.push(element)
+        }
+    }
+    return [...quickSort(left), middleItem, ...quickSort(right)];
 }
