be4e7ac19820670b9c91c4ae1899379ba8da431f
cumt-robin <1148121254@qq.com>
2021-04-24 15:20:41 +0800

    feat: isPerfectSquare

--- 代码/数据结构和算法/Search/easy/isPerfectSquare.js
+++ 代码/数据结构和算法/Search/easy/isPerfectSquare.js
@@ -1,10 +1,24 @@
 
 // https://leetcode-cn.com/problems/valid-perfect-square/
 
+
 /**
+ * 这个比mySqrt还简单点，不需要取整。
  * @param {number} num
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-
+    let left = 1, right = num;
+    while (left <= right) {
+        let mid = left + ((right - left) >> 1)
+        let product = mid * mid;
+        if (product === num) {
+            return true;
+        } else if (product < num) {
+            left = mid + 1
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
 };
--- 代码/数据结构和算法/Search/easy/solution.js
+++ 代码/数据结构和算法/Search/easy/solution.js
@@ -16,8 +16,9 @@
  * 2126753390
  * 1702766719
  * 其实可以只判断 middle is bad，并且一直往左边收紧
- * 我是用 (left + right) >> 1 求取 middle 的索引的，最后在评论里翻到一条评论，终于知道我错在哪里了，实际上会 left + right 会溢出。
- * 但是我最后还是超时了，结果发现把 (right - left) >> 1 改为 Math.floor((right - left) / 2)就不超时了，我特么，难道右移不是更快吗？
+ * 我是用 (left + right) >> 1 求取 middle 的位置的，最后在评论里翻到一条评论，终于知道我错在哪里了，实际上会 left + right 会溢出。
+ * 但是我最后还是超时了，结果发现把 left + (right - left) >> 1 改为 Math.floor((right - left) / 2)就不超时了，我特么，难道右移不是更快吗？
+ * 原来问题不是 >> 1 慢，而是 + 的运算优先级高于 >>，这...
  * @param {function} isBadVersion()
  * @return {function}
  */
@@ -29,7 +30,7 @@ var solution = function(isBadVersion) {
     return function(n) {
         let left = 1, right = n;
         while (left < right) {
-            let middle = left + (right - left) >> 1;
+            let middle = left + ((right - left) >> 1);
             if (isBadVersion(middle)) {
                 right = middle
             } else {
@@ -39,3 +40,26 @@ var solution = function(isBadVersion) {
         return left;
     };
 };
+
+// 经验证，我最开始的解法还是更快的
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1, right = n;
+        while (left <= right) {
+            let middle = left + ((right - left) >> 1);
+            let isMiddleBad = isBadVersion(middle)
+            let isLastBad = isBadVersion(middle - 1)
+            if (!isLastBad && isMiddleBad) {
+                return middle;
+            } else if (isMiddleBad) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+    }
+}
