7faa692b9c676e8bcf4a70182934504874a33686
cumt-robin <1148121254@qq.com>
2021-04-27 00:24:55 +0800

    feat: hanota临时代码

--- 代码/数据结构和算法/Recursion/easy/hanota.js
+++ 代码/数据结构和算法/Recursion/easy/hanota.js
@@ -2,11 +2,21 @@
 // https://leetcode-cn.com/problems/hanota-lcci/
 
 /**
+ * 汉诺塔问题利用了栈的数据结构，同时使用分治思想分解子问题
+ * 
  * @param {number[]} A
  * @param {number[]} B
  * @param {number[]} C
  * @return {void} Do not return anything, modify C in-place instead.
  */
 var hanota = function(A, B, C) {
-
+    function move(n, A, B, C) {
+        if (n === 1) {
+            C.push(A.pop())
+        } else {
+            move(n - 1, A, C, B)
+            move(1, A, B, C)
+        }
+    }
+    move(A.length, A, B, C)
 };
