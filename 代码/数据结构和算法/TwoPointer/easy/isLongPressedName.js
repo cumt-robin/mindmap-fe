ff79997bc4e1c4e120b629c59ad1708d28e57783
cumt-robin <1148121254@qq.com>
2021-05-18 00:14:21 +0800

    chore: isLongPressedName部分代码

--- 代码/数据结构和算法/TwoPointer/easy/isLongPressedName.js
+++ 代码/数据结构和算法/TwoPointer/easy/isLongPressedName.js
@@ -14,11 +14,37 @@
  * @param {string} typed
  * @return {boolean}
  */
-var isLongPressedName = function(name, typed) {
+ var isLongPressedName = function(name, typed) {
     const len1 = name.length, len2 = typed.length;
     // 先通过长度对比，把一些容易判断的场景筛掉
     if (len1 > len2) {
         return false;
     }
-    
+    let checkIndex = 0;
+    let checkIndex2 = 0;
+    let repeat = 0;
+    // 问题是最后一波repeat统计完了，就出循环了。。。
+    for (let i = checkIndex; i < len1; i++) {
+        if (name[i] === name[checkIndex]) {
+            repeat++;
+        } else {
+            // 出现了不同，检查 typed
+            inner: for (let j = checkIndex2, repeat2 = 0; j < len2; j++) {
+                if (name[checkIndex] === typed[j]) {
+                    repeat2++;
+                } else if (repeat2 < repeat) {
+                    return false;
+                } else {
+                    // repeat2 >= repeat，满足条件，继续
+                    repeat = 0;
+                    checkIndex2 = j;
+                    checkIndex = i--;
+                    // 外循环正常执行，会发生 i++，所以要用 i--处理一下
+                    // 跳出内循环，此时未执行 j++
+                    break inner;
+                }
+            }
+        }
+    }
+    return repeat === 0;
 };
