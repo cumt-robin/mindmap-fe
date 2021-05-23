272bde20f4246d06349f06511dbb162b68089eb2
cumt-robin <1148121254@qq.com>
2021-05-23 18:07:26 +0800

    feat: isPalindrome新增解法

--- 代码/数据结构和算法/LinkedList/easy/isPalindrome.js
+++ 代码/数据结构和算法/LinkedList/easy/isPalindrome.js
@@ -34,3 +34,56 @@ var isPalindrome = function(head) {
     }
     return true;
 };
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+function arr2ListHead(arr) {
+    if (arr.length === 0) {
+        return null;
+    }
+    var dummy = {}
+    var i = 0;
+    var tempNode = dummy;
+    while(i < arr.length) {
+        var nextNode = new ListNode(arr[i])
+        tempNode.next = nextNode;
+        tempNode = nextNode;
+        i++;
+    }
+    return dummy.next;
+}
+
+var l1 = new arr2ListHead([1,1,2,1])
+
+/**
+ * 或者先反转链表，再对比
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let prev = null;
+    let curr = head;
+    const arr = [];
+    while(curr) {
+        arr.push(curr.val)
+        let next = curr.next;
+        // 前序 prev 节点变成 next 节点，实现反转
+        curr.next = prev;
+        // 用 prev 临时存储反转的头指针
+        prev = curr;
+        curr = next;
+    }
+    // 此时 prev 已经是反转链表的头指针了
+    let i = 0;
+    while(prev) {
+        if (prev.val !== arr[i]) {
+            return false;
+        }
+        prev = prev.next;
+        ++i;
+    }
+    return true;
+};
