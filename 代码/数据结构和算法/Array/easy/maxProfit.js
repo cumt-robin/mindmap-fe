49099cd6726f72f47a59d66494cd91c45e0b3b18
cumt-robin <1148121254@qq.com>
2021-06-03 23:32:30 +0800

    chore: maxProfit基本思路

---
+++ 代码/数据结构和算法/Array/easy/maxProfit.js
@@ -0,0 +1,13 @@
+// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2zsx1/
+
+/**
+ * 买卖股票的最佳时机
+ * 跌到底的时候买（账户扣钱），涨到顶的时候卖（账户加钱）
+ * 如果一直跌，不买
+ * 如果一直涨，最早的时候买，最后的时候卖
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+};
